a179c779bc5e9266db84f16b4009b022dfd3a627
Cassie Jingyi <jingyi.zhang05@gmail.com>
2021-02-18 00:55:28 -0800

    Create # 169. Majority Element.java

---
+++ # 169. Majority Element.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        HashMap <Integer, Integer> map = new HashMap <Integer, Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) &&
+                map.get(nums[i]) + 1 > nums.length/2) {
+                return nums[i];
+            } else {
+                map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
+            }
+        }
+        return -1;
+    }
+}
+
+// Enhanced for loop
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        HashMap <Integer, Integer> map = new HashMap <Integer, Integer>();
+        
+        // for-each element
+        for (int i: nums) {
+            if (map.containsKey(i) &&
+                map.get(i) + 1 > nums.length/2) {
+                return i;
+            } else {
+                map.put(i, map.getOrDefault(i,0) + 1);
+            }
+        }
+        return -1;
+    }
+}
